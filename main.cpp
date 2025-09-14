c55fad116e88e4ebb4208e4cab558c50924d17ee
Hiling007 <1658456795@qq.com>
2025-09-14 11:20:36 +0800

    upload the bag

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// #include "test_h.h"
+// #include "opencv4/opencv2/core/core.hpp"
+#include <opencv4/opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    cout<<CV_VERSION;
+    return 0;
+}
